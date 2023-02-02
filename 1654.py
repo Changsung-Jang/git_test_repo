69b73ea5f86387ef1055fb5feaa6a518910c18cf
Changsung-Jang <ericjang2002@snu.ac.kr>
2023-02-02 07:13:14 +0000

    update

--- 1654.py
+++ 1654.py
@@ -1,13 +1,42 @@
 K, N = map(int, input().split())
-lines = [0]*K
+lines = [0] * K
 for i in range(K):
     lines[i] = int(input())
 
 lines_sum = sum(lines)
 max_lines = max(lines)
-init_start = max_lines // N
-init_end = lines_sum // N
 
+init_start = max_lines // N
+init_end = max_lines #랜선 길이 합친 것을 N으로 나눈 것보단 답이 작을 것
+
+maxim = 0 #구하려는 답의 최댓값
 def bin_search(start, end):
-    min = (start + end) // 2
-    
+    global maxim
+    numbers = 0 
+
+    if start == end:
+        maxim = start
+        return
+    elif start == end-1: #해당 조건 만들어주지 않으면 무한 루프 생성
+        for line in lines:
+            numbers += line // end
+            if numbers >= N:
+                maxim = end
+            else:
+                maxim = start
+        return 
+
+    mid = (start + end) // 2
+
+    for line in lines:
+        numbers += line // mid
+
+    if numbers < N: #조각이 적다 -> 길이는 길다
+        bin_search(start, mid-1) #최댓값을 찾는 것인데 mid는 만족하지 못하므로 포함 X
+
+    elif numbers >= N:
+        bin_search(mid, end) #mid가 최댓값일 수도 있다
+
+bin_search(init_start, init_end)
+
+print(maxim)
--- 3079.py
+++ 3079.py
@@ -1,1 +1,31 @@
+T = []
 N, M = map(int, input().split())
+for i in range(N):
+    T.append(int(input()))
+
+start = 1; end = min(T) * M
+#print("시작은 ", start, end)
+
+while end - start > 1:
+    mid = (start + end) // 2
+    people = 0
+    for control in T:
+        people += mid//control
+    
+    if people < M:
+        start = mid + 1
+    else:
+        end = mid
+
+#print(start, end)
+if start == end:
+    print(end)
+else:
+    people = 0
+    for control in T:
+        people += start//control
+    
+    if people >= M:
+        print(start)
+    else:
+        print(end)
