a1416d246bb8f000561dda795ca7c33895625d4d
Changsung-Jang <ericjang2002@snu.ac.kr>
2023-02-03 13:28:09 +0000

    dde

--- 1197.py
+++ 1197.py
@@ -2,7 +2,7 @@ import heapq
 from collections import defaultdict
 #중요: heapq에 push를 할 때 튜플을 사용할 경우 튜플의 첫 번째 항목을 기준으로 우선수위를 가지게 된다.
 V, E = map(int, input().split())
-S = []
+S = [1]
 heap_q = []
 edge_table = defaultdict(list)
 for i in range(E):
@@ -12,7 +12,23 @@ for i in range(E):
 
     if 1 < i <= V:
         heapq.heappush(heap_q, (1000001, i))
-
+    
+#while S == T:
+    
 
 
 print(heap_q)
+print(edge_table)
+
+'''
+test case
+1 2 8
+2 3 10
+1 4 9
+4 3 5
+1 5 11
+5 4 13
+4 7 12
+5 6 8
+6 7 7
+'''
